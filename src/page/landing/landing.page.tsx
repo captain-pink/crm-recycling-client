51c58a7601f207358f6d852fa68f37a59880f5e4
Roman Pochanin <roman@codesignal.com>
2023-11-11 22:31:35 +0200

    Finalize the landing page

--- src/page/landing/landing.page.tsx
+++ src/page/landing/landing.page.tsx
@@ -4,14 +4,45 @@ import Typography from "@mui/material/Typography";
 import CardMedia from "@mui/material/CardMedia";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
-import { StepCard } from "../../component/step-card/step.card.tsx";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { MenuLink } from "../../component/menu-link/menu.link.tsx";
+import { StepCard } from "../../component/step-card/step.card.tsx";
 
 export const LandingPage = () => {
+  const navigate = useNavigate();
 
   return (
-    <Container sx={{ marginTop: '4rem' }}>
+    <Box sx={{ position: 'relative' }}>
+      <Container>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '4rem' }}>
+
+          <CardMedia
+            loading="lazy"
+            component="img"
+            onClick={() => navigate('/')}
+            sx={{ width: '100px', objectFit: 'contain', objectPosition: 'center', cursor: 'pointer' }}
+            image="/public/logo.png"
+          />
+
+          <Box sx={{ display: 'flex', gap: '1.5rem', padding: '1.5rem 0' }}>
+            <MenuLink url={'/'} title={'Home.'}/>
+            <MenuLink url={'/'} title={'About Us.'}/>
+            <MenuLink url={'/'} title={'How it works.'}/>
+            <MenuLink url={'/'} title={'Manufacturers.'}/>
+            <MenuLink url={'/'} title={'Recycling Centers.'}/>
+          </Box>
+
+          <Box sx={{ display: 'flex', gap: '1rem' }}>
+            <Button onClick={() => navigate('/login')} size={'large'} color="primary">
+              Login
+            </Button>
+
+            <Button onClick={() => navigate('/sign-up')} size={'large'} color="primary" variant="contained">
+              Get Started
+            </Button>
+          </Box>
+        </Box>
+
       <Grid container justifyContent={'space-between'}>
         <Grid item lg={5.8} xl={5.8} sx={{display: 'flex', flexDirection: 'column', gap: '2rem'}}>
           <CardMedia
@@ -86,11 +117,147 @@ export const LandingPage = () => {
         Demonstrate your commitment to the environment.
       </Typography>
 
+      <Grid container justifyContent={'space-between'} marginTop={'2.5rem'}>
+          <Grid item lg={5.8} xl={5.8} sx={{ position: 'relative', display: 'flex', flexDirection: 'column', gap: '2rem', overflow: 'hidden' }}>
+            <Box
+              sx={{
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                position: 'absolute',
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '2rem',
+                backgroundColor: '#D3FBD8',
+                borderRadius: '1rem',
+                padding: '2rem 1.5rem 0 1.5rem',
+                justifyItems: 'flex-start',
+              }}
+            >
+              <Typography color={'primary'} variant="h1" fontWeight={600} fontSize={'2rem'} textAlign={'center'}>
+                Track lifecycle of your devices
+              </Typography>
+
+              <Box sx={{
+                display: 'flex',
+                justifyContent: 'center',
+              }}>
+                <Button size={'large'} variant="contained" color="primary">
+                  Join now
+                </Button>
+              </Box>
+
+              <CardMedia
+                loading="lazy"
+                component="img"
+                image={'/public/image-11.png'}
+                sx={{
+                  position: 'absolute',
+                  bottom: 0,
+                  alignSelf: 'center',
+                  transform: 'translateX(32px)',
+                  width: '75%',
+                  objectFit: 'contain',
+                  objectPosition: 'center',
+                  borderRadius: '1rem',
+                }}
+              />
+            </Box>
+          </Grid>
+          <Grid item lg={5.8} xl={5.8} overflow={'hidden'} sx={{ borderRadius: '1rem', padding: 0 }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+              <Box
+                sx={{
+                  position: 'relative',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  gap: '1rem',
+                  backgroundColor: '#C9F7FF',
+                  borderRadius: '1rem',
+                  padding: '2rem 1.5rem',
+                  minHeight: '230px'
+                }}
+              >
+                <CardMedia
+                  loading="lazy"
+                  component="img"
+                  image={'/public/frame-38.png'}
+                  sx={{
+                    position: 'absolute',
+                    bottom: '1.5rem',
+                    top: 0,
+                    right: 0,
+                    width: '45%',
+                    objectFit: 'contain',
+                    objectPosition: 'center',
+                  }}
+                />
+                <Box sx={{width: '55%', display: 'flex', flexDirection: 'column', gap: '1rem'}}>
+                  <Typography color={'primary'} variant="h1" fontWeight={600} fontSize={'2rem'} >
+                    Highlight recyclable materials
+                  </Typography>
+                  <Typography
+                    color={"#000000DE"}
+                    variant="body1"
+                    fontSize="1rem"
+                    fontWeight={400}
+                    sx={{ textWrap: 'balance' }}
+                  >
+                    Showcase the environmentally conscious design of your devices, emphasizing recyclable components and responsible manufacturing.
+                  </Typography>
+                </Box>
+              </Box>
+
+              <Box
+                sx={{
+                  position: 'relative',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  gap: '1rem',
+                  backgroundColor: '#DAF0EB',
+                  borderRadius: '1rem',
+                  padding: '2rem 1.5rem',
+                  minHeight: '230px'
+                }}
+              >
+                <CardMedia
+                  loading="lazy"
+                  component="img"
+                  image={'/public/leaf.png'}
+                  sx={{
+                    position: 'absolute',
+                    bottom: 0,
+                    right: 0,
+                    width: '45%',
+                    objectFit: 'contain',
+                    objectPosition: 'center',
+                  }}
+                />
+                <Box sx={{width: '55%', display: 'flex', flexDirection: 'column', gap: '1rem'}}>
+                  <Typography color={'primary'} variant="h1" fontWeight={600} fontSize={'2rem'} >
+                    Gain recognition for responsible manufacturing practices
+                  </Typography>
+                  <Typography
+                    color={"#000000DE"}
+                    variant="body1"
+                    fontSize="1rem"
+                    fontWeight={400}
+                    sx={{ textWrap: 'balance' }}
+                  >
+                    Receive accolades for your commitment to the environment. Stand out as an industry leader in responsible and sustainable production.
+                  </Typography>
+                </Box>
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+
 
       <Box height={'10rem'}/>
 
       <Box
-        sx={{ backgroundImage: 'url("/public/image-18.png")', display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+        sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
         <Typography color={'primary'} variant="h1" fontWeight={600} fontSize={'3rem'} textAlign={'center'}>
           How It Works
         </Typography>
@@ -122,7 +289,7 @@ export const LandingPage = () => {
         </Box>
       </Box>
 
-      <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '1.5rem' }}>
+      <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '3rem' }}>
         <Button size={'large'} variant="contained" color="primary">
           Explore how it works
         </Button>
@@ -219,7 +386,8 @@ export const LandingPage = () => {
         <CardMedia
           loading="lazy"
           component="img"
-          sx={{width: '100px', objectFit: 'contain', objectPosition: 'center'}}
+          onClick={() => navigate('/')}
+          sx={{ width: '100px', objectFit: 'contain', objectPosition: 'center', cursor: 'pointer' }}
           image="/public/logo.png"
         />
 
@@ -253,6 +421,22 @@ export const LandingPage = () => {
         </Box>
       </Box>
 
+        <Box
+          sx={{
+            position: 'absolute',
+            bottom: '0',
+            left: '0',
+            right: '0',
+            width: '100%',
+            height: '100vh',
+            backgroundImage: 'url("/public/image-18.png")',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            zIndex: -1,
+          }}
+        />
+
     </Container>
+    </Box>
   )
 }
